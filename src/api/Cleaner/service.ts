5b44d26fde81fe2a60fb393ae708168817134d81
Agbo Frank <agbofrank531@gmail.com>
2024-05-18 21:06:52 +0100

    adde the proile method of cleaner service

--- src/api/Cleaner/service.ts
+++ src/api/Cleaner/service.ts
@@ -1,4 +1,8 @@
+import Cleaner from "../../model/cleaner"
 import Order from "../../model/order"
+import { compareStrings } from "../../utility/helpers"
+import { NotFoundException } from "../../utility/service-error"
+import { FilterQuery } from "mongoose"
 
 class Service {
 
@@ -8,11 +12,77 @@ class Service {
     return {message: "Orders retreved successfully", data}
   }
 
+  async cancel(id: string, user: string){
+    const order = await Order.findById(id)
+    if(!order) throw new NotFoundException("Order not found");
+    
+    await order.updateOne({$pull: { user }})
+
+    return { message: "Order declined successfully", data: null}
+  }
+
+  async accept(id: string, user: string){
+    const order = await Order.findById(id)
+    if(!order) throw new NotFoundException("Order not found");
+
+    const cleaner_order = order.cleaners.find(c => c.user === user)
+    cleaner_order.accepted = true
+    await order.save()
+
+    return { message: "Order accepted successfully", data: null}
+  }
   requestKit(){}
-  profile(){}
-  getCleaners(){}
+  async profile(user: string){
+    const data = await Cleaner.findOne({ user }).populate("user", "-password")
+
+    return {
+      message: "Cleaner's profile retrieved successfully",
+      data
+    }
+  }
+
+  async getCleaners(query){
+    const filters = this.filters(query)
+
+    const data = await Cleaner.find({ 
+      $and: filters.length > 0 ? filters : [{}]
+    }).populate("user", "-password")
+
+    return {
+      message: "Cleaners retrieved successfully",
+      data
+    }
+  }
+
   kyc(){
     //docs: proof of work, photo, gov id, background check
+  }
+
+  private filters(payload: any){
+    const allowed_filters = ["lat", "long"]
+    const queries: FilterQuery<any>[] = []
+    const _filters = Object.entries(payload)
+
+    for (let i = 0; i < _filters.length; i++) {
+      if(!allowed_filters.includes(_filters[i][0])) continue;
+      if(
+        compareStrings(_filters[i][0], "lat") ||
+        compareStrings(_filters[i][0], "long")
+      ){
+        queries.push({
+          location: { $near: {
+            $geometry: { 
+              type: 'Point',
+              coordinates: [payload?.long, payload?.lat]
+            },
+            $maxDistance: 5000  // Distance in meters
+          }}
+        })
+      }
+      else queries.push({[_filters[i][0]]: { $regex: new RegExp(`${_filters[i][1]}`), $options: "i"}})
+    }
+
+    return queries
   }
 }
 
--- src/model/cleaner.ts
+++ src/model/cleaner.ts
@@ -1,16 +1,27 @@
-import { Schema, model } from "mongoose";
+import { Schema, Types, model } from "mongoose";
 
 export interface ICleaner {
-  user: string
+  user: string | typeof Types.ObjectId
   code: string
   earnings: number
   verified: boolean
+  location: {
+    type: string
+    coordinates: Number []
+  }
 }
 
 const cleaner = new Schema<ICleaner>({
-  user: String,
+  user: {
+    type: Types.ObjectId,
+    ref: "user"
+  },
   code: String,
   earnings: Number,
+  location: {
+    type: {type: String, default: "Point"},
+    coordinates: [ Number ]
+  },
   verified: {
     type: Boolean,
     default: false
@@ -21,6 +32,8 @@ const cleaner = new Schema<ICleaner>({
     updatedAt: "updated_at"
   }
 })
+
+cleaner.index({ location: '2dsphere' });
 
 const Cleaner = model<ICleaner>("cleaner", cleaner)
 export default Cleaner
